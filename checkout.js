fc1b704f3a56fa550fd0d21da4922fa4b3f36d3e
Ayca96 <aycakubra1@gmail.com>
2024-06-12 12:02:09 +0200

    added total-price

--- checkout.js
+++ checkout.js
@@ -5,13 +5,13 @@
 // console.log(minusbtn);
 
 // minusbtn.forEach((minus)=>{
-//   minus.addEventListener("click",()=>{ 
+//   minus.addEventListener("click",()=>{
 //     console.log("-");
 //   })
 // })
 
 // plusbtn.forEach((plus)=>{
-//   plus.addEventListener("click",()=>{ 
+//   plus.addEventListener("click",()=>{
 //     console.log("+");
 //   })
 // })
@@ -23,23 +23,20 @@ const products = document.querySelector("article.products");
 //console.log(products);
 //console.log(deleteAllBtn);
 
-
 //? Event Listeners
 
-deleteAllBtn.addEventListener("click",()=>{
+deleteAllBtn.addEventListener("click", () => {
+  products.textContent = "No Product";
+  //console.log(products.classList );
+  products.classList.add("no-product"); // elemente class ekler.
+  //console.log(products.classList );
 
-products.textContent="No Product";
-//console.log(products.classList );
-products.classList.add("no-product"); // elemente class ekler.
-//console.log(products.classList );
+  //document.querySelector(".delete-div").style.display="none" display none sadece gizliyor kaldirmiyor. remove() kaldiriyor firekt.
+  document.querySelector(".delete-div").remove();
+});
 
-//document.querySelector(".delete-div").style.display="none" display none sadece gizliyor kaldirmiyor. remove() kaldiriyor firekt.
-document.querySelector(".delete-div").remove()
-
-})
-
-products.addEventListener("click",(e)=>{
-// contains() ayni includes() gibi. Icinde var mi varsa true  yoksa false. Burda includes kullanamayiz arraylerde kullaniyoruz. Ancak arraye cevirirsek kullanabiliriz.
+products.addEventListener("click", (e) => {
+  // contains() ayni includes() gibi. Icinde var mi varsa true  yoksa false. Burda includes kullanamayiz arraylerde kullaniyoruz. Ancak arraye cevirirsek kullanabiliriz.
 
   //todo Tek bir event listener ile tüm butonlari yakaladik.
 
@@ -55,20 +52,45 @@ products.addEventListener("click",(e)=>{
   //   console.log("Bosluga tikladiniz.");
   // }
 
-
-  if(e.target.classList.contains("fa-plus")){
-    //? static olarak kullanirsak hangi + butonuna basilirsa ilk quantity(miktar) artar.
-    //document.getElementById("quantity").textContent++
-    e.target.previousElementSibling.textContent++
-  }
-
-  else if(e.target.classList.contains("fa-minus")){
-
-    if (e.target.nextElementSibling.textContent >1) {
-      e.target.nextElementSibling.textContent--
+  if (e.target.classList.contains("fa-plus")) {
+    //? static olarak kullanırsak hangi + butonuna basılırsa basılsın ilk quantity artar.
+    // document.getElementById("quantity").textContent++
+    e.target.previousElementSibling.textContent++;
+    calculatePrice(e.target);
+  } else if (e.target.classList.contains("fa-minus")) {
+    if (e.target.nextElementSibling.textContent > 1) {
+      e.target.nextElementSibling.textContent--;
     }
-  } else if(e.target.classList.contains("fa-trash-can")){
-      e.target.closest(".product").remove()
+    calculatePrice(e.target);
+  } else if (e.target.classList.contains("fa-trash-can")) {
+    e.target.closest(".product").remove();
   }
+});
 
+const calculatePrice = (btn) => {
+  const discountedPrice = btn
+    .closest(".product-info")
+    .querySelector("#discounted-price");
+  // console.log(discountedPrice.textContent)
+  const productPrice = btn
+    .closest(".buttons-div")
+    .querySelector("#product-price");
+
+  const quantity = btn.parentNode.querySelector("#quantity");
+
+  productPrice.textContent = (
+    quantity.textContent * discountedPrice.textContent
+  ).toFixed(2);
+  totalPrice()
+};
+
+const totalPrice = () =>{
+  const prices = document.querySelectorAll("#product-price");
+  prices.forEach((price)=>{
+    console.log(price.textContent);
+  })
+}
+
+window.addEventListener("load", ()=>{
+  totalPrice()
 })
